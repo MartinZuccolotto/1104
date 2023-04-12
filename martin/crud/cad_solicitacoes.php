6d2edc18779c02a20e53fb3fcac89b2ac2d85a8c
MartinZuccolotto <101228566+MartinZuccolotto@users.noreply.github.com>
2023-04-11 21:35:20 -0300

    Add files via upload

---
+++ martin/crud/cad_solicitacoes.php
@@ -0,0 +1,22 @@
+<?php
+    include ('../conn.php');
+
+    $nome = $_POST['nome'];
+    $solicitacoes = $_POST['solicitacoes'];
+
+    if(empty($nome) || empty($solicitacoes)){
+        echo "Voce precida preencher todos os campos";
+    }else{
+        $cad_solicitacoes = $pdo->prepare('INSERT INTO solicitacoes (nome, solicitacoes) values (:nome, :solicitacoes)');
+        $cad_solicitacoes->execute(array(
+            ':nome'=>$nome,
+            ':solicitacoes'=>$solicitacoes,
+        )
+        );
+        
+        echo "<script>
+        alert('solicitacao Cadastrada!')
+        //window.location.href
+        </script>";
+    }
+?>
